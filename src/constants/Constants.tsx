249b8e9463ddf8973e5b734c709be7e29ab7c9f5
Dimax <dmitryourkevich@gmail.com>
2024-03-08 23:25:57 -0700

    constants, to hold urls, etc ..

---
+++ src/constants/Constants.tsx
@@ -0,0 +1,8 @@
+export const Constants = {
+	gameUrl: "http://localhost:8080/game",
+	indexUrl: "http://localhost:8080",
+	formHeaders: {
+		"Content-Type": "multipart/form-data",
+		'Accept': 'application/json',
+	  }
+}
