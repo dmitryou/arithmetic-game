3adb4597e66c4a74d9de0483c854a8757e4b9c18
Dimax <dmitryourkevich@gmail.com>
2024-03-08 23:25:24 -0700

    App into 2 screens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Game } from './src/screens/Game';
+import { Intro } from './src/screens/Intro';
 
 export default function App() {
+
+  const [isIntro, setIsIntro] = useState(true);
+
+  const changeScreens = () => {
+    setIsIntro(!isIntro);
+  }
+
+  const renderIntro = () => {
+    return (
+      <>
+        <Intro />
+        <Button title={'Start game'} onPress={changeScreens}></Button>
+      </>
+    );
+  }
+
+  const renderGame = () => {
+    return (
+      <>
+        <Game />
+        <Button title={'Back to intro'} onPress={changeScreens}></Button>
+      </>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      {
+        isIntro === true ? renderIntro() : renderGame()}
+    </View >
   );
 }
 
